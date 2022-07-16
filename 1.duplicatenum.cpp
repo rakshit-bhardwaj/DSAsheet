2b04674bb5cc663a47c1b13dad925ad99bab47f7
rakshit-bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2022-07-16 11:18:53 +0530

    Update 1.duplicatenum.cpp

--- 1.duplicatenum.cpp
+++ 1.duplicatenum.cpp
@@ -4,11 +4,16 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1])
-            return nums[i];
+        int slow1=0,slow2=0,fast=0;
+        do {
+            slow1 = nums[slow1];
+            fast = nums[nums[fast]];
+        } while(slow1 != fast);
+
+        while(slow1 != slow2){
+            slow1 = nums[slow1];
+            slow2 = nums[slow2];
         }
-        return -1;
+        return slow1;
     }
 };
