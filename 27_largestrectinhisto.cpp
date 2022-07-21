71c77ebc7ba71559b483461137d66f0045506f5f
rakshit-bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2022-07-21 22:29:28 +0530

    Add files via upload

---
+++ 27_largestrectinhisto.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+};
+
+int main (){
+    Solution obj;
+    vector<int> heights = {2,1,5,6,2,3};
+    cout<<obj.largestRectangleArea(heights);
+    return 0;
+}
