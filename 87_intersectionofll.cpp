1cbc7370c90a2297d49ac04fbe21db9e4966bcd8
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2022-08-14 14:01:35 +0530

    Update 87_intersectionofll.cpp

--- 87_intersectionofll.cpp
+++ 87_intersectionofll.cpp
@@ -13,14 +13,33 @@ struct ListNode
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*,int> addresses;
-        while(headA!=NULL){
-            addresses[headA] = headA->val;
-            headA=headA->next;
+        int c=0,c1=0,c2=0;
+        ListNode* p1 = headA;
+        ListNode* p2 = headB;
+        while(p1!=NULL){
+            c1++;
+            p1=p1->next;
         }
-        while(headB!=NULL){
-            if(addresses.find(headB)!=addresses.end()) return headB;
-            headB=headB->next;
+        while(p2!=NULL){
+            c2++;
+            p2=p2->next;
+        }
+        p1 = headA;
+        p2 = headB;
+        c = abs(c1-c2);
+        if(c1>c2){
+            for(int i=0;i<c;i++){
+                p1 = p1->next;
+            }
+        } else if(c2>c1) {
+            for(int i=0;i<c;i++){
+                p2 = p2->next;
+            }
+        }
+        while(p1!=NULL && p2!=NULL){
+            if(p1==p2) return p1;
+            p1=p1->next;
+            p2=p2->next;
         }
         return NULL;
     }
