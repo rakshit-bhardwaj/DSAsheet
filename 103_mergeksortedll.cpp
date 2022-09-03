c61549b0ee811ed8f7535c4a409b2cfe98ea5af7
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2022-09-03 23:13:39 +0530

    Add files via upload

---
+++ 103_mergeksortedll.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* llmid(ListNode* head){
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* left,ListNode* right){
+        if(left == NULL) return right;
+        if(right == NULL) return left;
+        
+        ListNode* ans = new ListNode();
+        ListNode* temp = ans;
+        while(left!=NULL && right!=NULL){
+            if(left->val < right->val){
+                temp->next = left;
+                temp = temp->next;
+                left = left->next;
+            } else {
+                temp->next = right;
+                temp = temp->next;
+                right = right->next;
+            }   
+        }
+
+        while(left!=NULL){
+            temp->next = left;
+            left = left->next;
+            temp = temp->next;
+        }
+        while(right!=NULL){
+            temp->next = right;
+            right = right->next;
+            temp = temp->next;
+        }
+
+        return ans->next;
+    }
+
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+        ListNode* left = head;
+        ListNode* mid = llmid(head);
+        ListNode* right = mid->next;
+        mid->next = NULL;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        ListNode* result = merge(left,right);
+        return result;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        if(lists.size()==1) return lists[0];
+        ListNode* head = new ListNode();
+        ListNode* ptr = head;
+        for(auto i :  lists){
+            ListNode* n = i;
+            while(n!=NULL){
+                ptr->next = n;
+                ptr = ptr->next;
+                n = n->next;
+            }
+        }
+        head = head->next;
+        head = sortList(head);
+        return head;
+    }
+};
