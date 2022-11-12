8b298b093073174169f8b37ad719925dfb3e9da8
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2022-11-12 23:12:52 +0530

    Add files via upload

---
+++ 124_opt_lcabst.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(p->val<root->val && q->val<root->val) return lowestCommonAncestor(root->left,p,q); 
+        else if(p->val>root->val && q->val>root->val) return lowestCommonAncestor(root->right,p,q); 
+        else return root;      
+    }
+};
