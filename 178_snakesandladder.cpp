b2c94e69f0b3a2331950827317964339beb4ea62
Rakshit Bhardwaj <100684015+rakshit-bhardwaj@users.noreply.github.com>
2023-02-03 23:36:44 +0530

    Update 178_snakesandladder.cpp

--- 178_snakesandladder.cpp
+++ 178_snakesandladder.cpp
@@ -1,77 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    map<int,pair<int,int>> m;
-
-    void converttocord(int n){
-        int num = 1;
-        int c = 1;
-        for(int i =n-1;i>=0;i--){
-            if(c%2!=0){
-                for(int j = 0;j<n;j++){
-                    m[num] = {i,j};
-                    num++;
-                }
+    int snakesAndLadders(vector<vector<int>> &board)
+    {
+        int row, column;
+        int n = board.size(), lbl = 1;
+        vector<pair<int, int>> cells(n * n + 1);
+        vector<int> columns(n);
+        iota(columns.begin(), columns.end(), 0);
+        for (int row = n - 1; row >= 0; row--)
+        {
+            for (int column : columns)
+            {
+                cells[lbl++] = {row, column};
             }
-            else {
-                for(int j = n-1;j>=0;j--){
-                    m[num] = {i,j};
-                    num++;
-                }
-            }
-            c++;
+            reverse(columns.begin(), columns.end());
         }
-    }
-
-    int snakesAndLadders(vector<vector<int>>& board) {
-        int n = board.size();
-        converttocord(n);
+        vector<int> dist(n * n + 1, -1);
+        dist[1] = 0;
         queue<int> q;
         q.push(1);
-        map<int,int> vis;
-        vis[1] = 1;
-        int ans = INT_MAX;
-        int count =0;
-        while(!q.empty()){
-            int size = q.size();
-            for(int k =0;k<size;k++){
-                int cur = q.front();
-                q.pop(); 
-                cout<<"current -> "<<cur<<" count -> "<<count<<endl;
-                if(cur==n*n) ans = min(count,ans);
-                pair<int,int> cord = m[cur];
-                if(board[cord.first][cord.second]!=-1){
-                    cout<<cur<<" = "<<board[cord.first][cord.second]<<endl;
-                    cur = board[cord.first][cord.second];
-                    if(cur==n*n) ans = min(count,ans);
-                    if(vis[cur]) continue;
-                    cout<<"current -> "<<cur<<" count -> "<<count<<endl;
-                } 
-            
-                for(int i=cur+1;i<=min(cur + 6,n*n);i++){
-                    if(!vis[i]){
-                        vis[i] = 1;
-                        q.push(i);
-                    }
+        while (!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            for (int next = curr + 1; next <= min(curr + 6, n * n); next++)
+            {
+                row = cells[next].first;
+                column = cells[next].second;
+                int destination = board[row][column] != -1 ? board[row][column] : next;
+                if (dist[destination] == -1)
+                {
+                    dist[destination] = dist[curr] + 1;
+                    q.push(destination);
                 }
             }
-            count ++;
         }
-        if(ans==INT_MAX) return -1;
-        return ans;
+        return dist[n * n];
     }
 };
-
-
-
--1,  1,  2, -1
- 2, 13, 15, -1
--1, 10, -1, -1
--1,  6,  2,  8
-
-16 15 14 13 
- 9 10 11 12
- 8  7  6  5 
- 1  2  3  4
